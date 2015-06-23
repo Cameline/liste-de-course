fc83a963fd268df5c9fc8428f703c190d5b11986
xbgv501 <xbgv501@444150AS095.corp.log.intra.laposte.fr>
2015-06-23 13:24:59 +0200

    ajout couleur de fond

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!doctype html>
-<html lang="fr">
-<head>
-<meta charset="utf-8">
-<title>LISTE DE COURSE</title>
-<link rel="stylesheet" href="style.css">
-<script src="script.js"></script>
-</head>
-<body>
-	<h1>Liste de course</h1>
-	<div id="nouvelarticle">
-		Nom article: <input type="text" name="nom" id="nom"><br>
-		<button type="button" onclick="ajouterArticle()">Ajouter</button>
-	</div>
-	<div id="listeArticle"></div>
-</body>
-</html>
+<?php
+include_once("home.html");
+?>
